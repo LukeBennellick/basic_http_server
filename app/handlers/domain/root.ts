ed3acd30e5619f7f4aac31dba145ffa6997d69e0
luke bennellick <luke.bennellick@usebloom.com>
2025-01-01 15:48:10 +0000

    Add content length header

--- app/handlers/domain/root.ts
+++ app/handlers/domain/root.ts
@@ -1,6 +1,8 @@
 import { CRLF, HTTP_VERSION, STATUS_200 } from "../../shared/constants";
 
 export const rootHandler = async (requestData = "") => {
-  const response = `${HTTP_VERSION} ${STATUS_200}${CRLF}Content-Type: text/plain${CRLF}${CRLF} Root Path`;
+  const body = "Root Path";
+  const contentLength = body.length;
+  const response = `${HTTP_VERSION} ${STATUS_200}${CRLF}Content-Length: ${contentLength}${CRLF}Content-Type: text/plain${CRLF}${CRLF}Root Path`;
   return response;
 };
