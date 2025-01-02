473072eb83e3a6bb0efdf931e92836cc821ea63f
luke bennellick <luke.bennellick@usebloom.com>
2025-01-02 12:30:21 +0000

    Implement basic handler into main

--- app/main.ts
+++ app/main.ts
@@ -5,11 +5,37 @@ import { parseVerbAndBasePathFromRequestBuffer } from "./utils/http";
 
 const PORT = 3000;
 
+export type Handlers = {
+  [key: string]: (
+    verb: string,
+    requestData: string,
+    opts?: object
+  ) => Promise<string> | Promise<Buffer>;
+};
+
+const handlers: Handlers = {
+  "/": (verb: string, incomingData: string) => {
+    switch (verb) {
+      case "GET": {
+        console.log("/ path");
+        const response = rootHandler(incomingData);
+        return response;
+      }
+      default:
+        console.log("Default path");
+        const response = rootHandler(incomingData);
+        return response;
+    }
+  },
+};
+
 const server = net.createServer((socket) => {
   socket.on("data", async (data: Buffer) => {
     const { verb, basePath } = parseVerbAndBasePathFromRequestBuffer(data);
 
-    console.log("We should now have the verb and basepath", verb, basePath);
+    const handler = handlers[basePath];
+    const response = await handler(verb, data.toString());
+    socket.write(response);
     socket.end();
   });
 
